9f3261f0266495db1b7d202a029d8a3416dbe74e
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2021-09-08 13:18:20 +0300

    Настройки проекта

--- TestCookie/TestCookie/Form1.cs
+++ TestCookie/TestCookie/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
 
 namespace TestCookie
 {
@@ -13,6 +14,13 @@ namespace TestCookie
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // https://professorweb.ru/my/csharp/web/level7/7_4.php
+            // https://qna.habr.com/q/366420
+            Cookie cookie = new Cookie("MyName", "Alex", "/", "localhost");
         }
     }
 }
