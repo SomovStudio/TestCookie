c9b13af92156a7f718987b6095102fce53c8e215
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2021-10-22 12:51:39 +0300

    Чтение cookie

--- linux/TestCookie/src/main/java/MainForm.java
+++ linux/TestCookie/src/main/java/MainForm.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
 
 public class MainForm {
     private JPanel MainPanel;
@@ -9,6 +11,7 @@ public class MainForm {
     private JTabbedPane tabbedPane1;
     private JMenu MenuAcrion;
     private JMenuItem MenuGetCookie;
+    private JTextPane textPane1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("MainForm");
@@ -29,7 +32,29 @@ public class MainForm {
         MenuGetCookie.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    CookieManager cookieManager = new CookieManager();
+                    CookieHandler.setDefault(cookieManager);
 
+                    URL url = new URL("https://www.google.com/");
+                    URLConnection urlConnection = url.openConnection();
+                    urlConnection.getContent();
+
+                    CookieStore cookieStore = cookieManager.getCookieStore();
+                    String result = "";
+                    for (HttpCookie cookie : cookieStore.getCookies()) {
+                        result = result + "\n Cookie: " + cookie.getName();
+                        result = result + "\t Domain: " + cookie.getDomain();
+                        result = result + "\t Value: " + cookie.getValue();
+                        //System.out.println("\n Cookie: " + cookie.getName());
+                        //System.out.println("\t Domain: " + cookie.getDomain());
+                        //System.out.println("\t Value: " + cookie.getValue());
+                    }
+
+                    textPane1.setText(result);
+                } catch (IOException malformedURLException) {
+                    malformedURLException.printStackTrace();
+                }
             }
         });
     }
