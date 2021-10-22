5ae1ff5fb798bfd9aaa0584d01ed02f102ffd909
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2021-10-22 11:46:30 +0300

    Проект под Linux

---
+++ linux/TestCookie/src/main/java/MainForm.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainForm {
+    private JPanel MainPanel;
+    private JMenu MenuFile;
+    private JMenuItem MenuClose;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(new MainForm().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("TestCookie");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MainForm() {
+        MenuClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
